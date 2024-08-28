05ba40a71567b7e74b232e0693f8e6e232bb0984
NuoFang <991921514@qq.com>
2024-08-28 11:09:57 +0800

    fix

---
+++ argo/argo.sh
@@ -0,0 +1,1 @@
+nohup ./cloudflared tunnel --edge-ip-version auto --protocol auto --logfile ./argo.log --loglevel debug --no-autoupdate run --token eyJhIjoiZmJhNzQyOGI2OWRiNTdhOTMwODNhMmNhNzUxZWUwMzIiLCJ0IjoiOTBmOGFiMDgtMDBmZS00ZTNiLTk1NTYtYWIwNmI2NzAyOGQ1IiwicyI6Ik9UTTBZVFptWWpVdFlUWXdNaTAwTmpsbUxXRTNNR1V0TWpJM1l6UXdZelV3TVdKayJ9 &
