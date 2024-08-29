c354d67b53063e2602bffa1bf88ce7aff6f81567
NuoFang <991921514@qq.com>
2024-08-29 10:38:45 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-nohup ./singbox/sing-box run &
-nohup ./argo/cloudflared tunnel --edge-ip-version auto --protocol auto --logfile ./argo.log --loglevel debug --no-autoupdate run --token eyJhIjoiZmJhNzQyOGI2OWRiNTdhOTMwODNhMmNhNzUxZWUwMzIiLCJ0IjoiOTBmOGFiMDgtMDBmZS00ZTNiLTk1NTYtYWIwNmI2NzAyOGQ1IiwicyI6Ik9UTTBZVFptWWpVdFlUWXdNaTAwTmpsbUxXRTNNR1V0TWpJM1l6UXdZelV3TVdKayJ9 &
+cd ./singbox
+nohup ./sing-box run &
+cd ../argo
+nohup ./cloudflared tunnel --edge-ip-version auto --protocol auto --logfile ./argo.log --loglevel debug --no-autoupdate run --token eyJhIjoiZmJhNzQyOGI2OWRiNTdhOTMwODNhMmNhNzUxZWUwMzIiLCJ0IjoiOTBmOGFiMDgtMDBmZS00ZTNiLTk1NTYtYWIwNmI2NzAyOGQ1IiwicyI6Ik9UTTBZVFptWWpVdFlUWXdNaTAwTmpsbUxXRTNNR1V0TWpJM1l6UXdZelV3TVdKayJ9 &
+cd ..
